4e2ea84fe9a34755fca8aab2a5a8c8b659f36017
Manuel Santos <2023110848@uac.pt>
2026-03-18 22:35:36 -0100

    Add files via upload

---
+++ LAB2/Calculadora_3/servidor/Operacoes/sqrt.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+
+class Raizquadrada:
+    def __init__(self):
+        self.res = 0
+
+    def executar(self, a:float)->Union[float,str]:
+        self.res= a ** 0.5
+        return self.res
+
+
