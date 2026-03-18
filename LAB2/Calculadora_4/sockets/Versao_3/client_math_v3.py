9e6be9179f36b7c8b1433b1840c23ad9a234b138
Manuel Santos <2023110848@uac.pt>
2026-03-18 22:49:21 -0100

    Add files via upload

---
+++ LAB2/Calculadora_4/sockets/Versao_3/client_math_v3.py
@@ -0,0 +1,93 @@
+import socket
+import json
+
+COMMAND_SIZE = 9
+INT_SIZE = 8
+ADD_OP = "add      "
+OBJ_OP = "add_obj  "
+SYM_OP = "sym      "
+BYE_OP = "bye      "
+SUB_OP = "sub      "
+END_OP = "stop     "
+PORT = 50005
+SERVER_ADDRESS = "localhost"
+
+# ----- enviar e receber strings ----- #
+def receive_str(connect, n_bytes: int) -> str:
+        """
+        :param n_bytes: The number of bytes to read from the current connection
+        :return: The next string read from the current connection
+        """
+        data = connect.recv(n_bytes)
+        return data.decode()
+
+def send_str(connect, value: str) -> None:
+
+    connect.send(value.encode())
+
+def send_int(connect:socket.socket, value: int, n_bytes: int) -> None:
+
+    connect.send(value.to_bytes(n_bytes, byteorder="big", signed=True))
+
+def receive_int(connect: socket.socket, n_bytes: int) -> int:
+
+        data = connect.recv(n_bytes)
+        return int.from_bytes(data, byteorder='big', signed=True)
+
+def send_object(connection, obj):
+ """1º: envia tamanho, 2º: envia dados."""
+ data = json.dumps(obj).encode('utf-8')
+ size = len(data)
+ send_int(connection, size, INT_SIZE) # Envio do tamanho
+ connection.send(data) # Envio do objeto
+def receive_object(connection):
+ """1º: lê tamanho, 2º: lê dados."""
+ size = receive_int(connection, INT_SIZE) # Recebe o tamanho
+ data = connection.recv(size) # Recebe o objeto
+ return json.loads(data.decode('utf-8'))
+def main():
+    #Socket & ligação
+    connection = socket.socket()
+    connection.connect((SERVER_ADDRESS,PORT))
+    # Testar a operação de soma
+    a = 10
+    b = 15
+    send_str(connection,ADD_OP)
+    send_int(connection,a, INT_SIZE)
+    send_int(connection,b, INT_SIZE)
+    res = receive_int(connection,INT_SIZE)
+    print("O resultado da soma é:",res)
+    # Execute a new type of operation: OBJ_OP
+    print("\nA testar a operação OBJ_OP com um dicionário...")
+    send_str(connection, OBJ_OP)
+
+    pedido_dicionario = {"oper": "+", "op1": a, "op2": b}
+    print(f"A enviar o dicionário: {pedido_dicionario}")
+    send_object(connection, pedido_dicionario)
+
+    res_obj = receive_int(connection, INT_SIZE)
+    print("O resultado da soma de OBJ_OP é:", res_obj)
+    # Client sends a dictionary.
+    # Example: {"oper":"+","oper1":4,"oper2":5}
+    # It receives an integer a result of the operation
+    # ...
+    # Testar duas operações de subtração
+    for i in range(2):
+        a += 1
+        # Operação de subtração
+        send_str(connection,SUB_OP)
+        send_int(connection,a, INT_SIZE)
+        send_int(connection,b, INT_SIZE)
+        res = receive_int(connection,INT_SIZE)
+        print("O resultado da subtração é:",res)
+    # 1 Fechar a conexão apenas do lado do cliente ou...
+    send_str(connection,BYE_OP)
+    print("Connection is going to close...")
+    #connection.close()
+    # 2 Fechar a conexão do lado do cliente e do servidor
+    #send_str(connection,END_OP)
+    #connection.close()
+    
+
+if __name__=="__main__":
+    main()
---
+++ LAB2/Calculadora_4/sockets/Versao_3/server_math_v3.py
@@ -0,0 +1,139 @@
+import math
+import socket
+import json
+
+COMMAND_SIZE = 9
+INT_SIZE = 8
+ADD_OP = "add      "
+OBJ_OP = "add_obj  "
+SYM_OP = "sym      "
+SUB_OP = "sub      "
+BYE_OP = "bye      "
+END_OP = "stop     "
+PORT = 35000
+SERVER_ADDRESS = "0.0.0.0"
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((SERVER_ADDRESS, PORT))
+sock.listen(1)
+print(f"Servidor pronto no IP local e porto {PORT}")
+
+
+def receive_int(connection, n_bytes: int) -> int:
+    """
+    :param n_bytes: The number of bytes to read from the current connection
+    :return: The next integer read from the current connection
+    """
+    data = connection.recv(n_bytes)
+    return int.from_bytes(data, byteorder='big', signed=True)
+
+def send_int(connection, value: int, n_bytes: int) -> None:
+    """
+    :param value: The integer value to be sent to the current connection
+    :param n_bytes: The number of bytes to send
+    """
+    connection.send(value.to_bytes(n_bytes, byteorder="big", signed=True))
+
+def receive_str(connection, n_bytes: int) -> str:
+    """
+    :param n_bytes: The number of bytes to read from the current connection
+    :return: The next string read from the current connection
+    """
+    data = connection.recv(n_bytes)
+    return data.decode()
+
+def send_str(connection, value: str) -> None:
+    """
+    :param value: The string value to send to the current connection
+    """
+    connection.connection.send(value.encode())
+
+def send_object(connection, obj):
+ """1º: envia tamanho, 2º: envia dados."""
+ data = json.dumps(obj).encode('utf-8')
+ size = len(data)
+ send_int(connection, size, INT_SIZE) # Envio do tamanho
+ connection.send(data) # Envio do objeto
+def receive_object(connection):
+ """1º: lê tamanho, 2º: lê dados."""
+ size = receive_int(connection, INT_SIZE) # Recebe o tamanho
+ data = connection.recv(size) # Recebe o objeto
+ return json.loads(data.decode('utf-8'))
+
+def main():
+    """
+    Runs the server until the client sends a "terminate" action
+    """
+    s = socket.socket()
+    s.bind(('', PORT))
+    s.listen(1)
+    print("Waiting for clients to connect on port " + str(PORT))
+    keep_running = True
+    while keep_running:
+        print("On accept...")
+        connection, address = s.accept()
+        print("Client " + str(address) + " just connected")
+        last_request = False
+        #Recebe messagens...
+        while not last_request:
+            request_type = receive_str(connection,COMMAND_SIZE)
+            if request_type == ADD_OP:
+
+                a = receive_int(connection,INT_SIZE)
+                b = receive_int(connection,INT_SIZE)
+                print("Pediram para somar:",a,"+",b)
+                result = a + b
+                send_int(connection,result, INT_SIZE)
+            elif request_type == SUB_OP:
+                a = receive_int(connection,INT_SIZE)
+                b = receive_int(connection,INT_SIZE)
+                print("Pediram para subtrair:",a,"-",b)
+                result = a-b
+                send_int(connection,result, INT_SIZE)
+            elif request_type == OBJ_OP:
+                print("Recebido um pedido do tipo OBJ_OP.")
+
+                # Receber o dicionário
+                pedido = receive_object(connection)
+                print(f"Dicionário recebido: {pedido}")
+
+                # Extrair os valores do dicionário
+                operador = pedido["oper"]
+                valor1 = pedido["op1"]
+                valor2 = pedido["op2"]
+
+                # Testar o tipo de operação e calcular
+                if operador == "+":
+                    result = valor1 + valor2
+                    print("Servidor calculou a soma dos objetos:", result)
+                    send_int(connection, result, INT_SIZE)
+                elif operador == "-":
+                    result = valor1 - valor2
+                    print("Servidor calculou a subtração dos objetos:", result)
+                    send_int(connection, result, INT_SIZE)
+                elif operador == "*":
+                    result = valor1 * valor2
+                    print("Servidor calculou a multiplicação dos objetos:", result)
+                    send_int(connection, result, INT_SIZE)
+                elif operador == "/":
+                    result = valor1 / valor2
+                    print("Servidor calculou a divisão dos objetos:", result)
+                    send_int(connection, result, INT_SIZE)
+                elif operador == "sqrt":
+                    result = valor1 ** 0.5
+                    print("Servidor calculou a raíz quadrada dos objetos:", result)
+                    send_int(connection, result, INT_SIZE)
+            # A new type of operation. The server receives a dictionary.
+            # It returns an integer.
+            elif request_type == BYE_OP:
+                print("Last request...")
+                last_request = True
+                keep_running = False
+            elif request_type == END_OP:
+                last_request = True
+                keep_running = False
+    print("Stopping...")
+    s.close()
+    print("Server stopped")
+
+if __name__=="__main__":
+    main()
