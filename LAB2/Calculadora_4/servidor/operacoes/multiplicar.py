582dddeaddf65e9e225e7b3703c3eb447f855ef1
Manuel Santos <2023110848@uac.pt>
2026-03-18 22:43:54 -0100

    Add files via upload

---
+++ LAB2/Calculadora_4/servidor/operacoes/multiplicar.py
@@ -0,0 +1,10 @@
+from typing import Union
+
+
+class Multiplicar:
+    def __init__(self):
+        self.res = 0
+
+    def executar(self, a:float, b:float)->Union[float,str]:
+        self.res = a * b
+        return self.res
